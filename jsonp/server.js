1a44f0d97cd427c5b445e7d773f13b2572539a32
Sean <lleiiell@live.com>
2021-12-16 20:02:47 +0800

    update

--- jsonp/server.js
+++ jsonp/server.js
@@ -2,8 +2,10 @@ const express = require("express")
 
 const app = express()
 app.get('/user/list',(req,res,next) => {
+    //获取参数
     let {callback,pageNo,pageSize} =  req.query;
     console.log(pageNo,pageSize)
+
     let resData = [{'userId':"001"},{'userId':"002"},{'userId':"003"},{'userId':"004"}]
     res.send(`${callback}(${JSON.stringify(resData)})`)
 })
--- proxy/server.js
+++ proxy/server.js
@@ -1,34 +1,14 @@
 const express = require("express")
 const {createProxyMiddleware} = require("http-proxy-middleware")
-const fs =require("fs")
 
 const app = express()
-console.log(__dirname)
 app.use(express.static(__dirname+'/'))
 
 app.use('^/sa',createProxyMiddleware({
-    target:'http://click.suning.cn',
-    changeOrign:true,
-    // ssl:{
-    //     key: fs.readFileSync('privatekey.pem'), 
-    //     cert: fs.readFileSync('certificate.pem')  // SELF-SIGNED CERTIFICATE
-    //   }
-    // onProxyRes:(proxyRes,req,res)=>{
-    //     res.header('Access-Control-Allow-Origin', '*');
-    //     res.end(res)
-    //     console.log(proxyRes)
-    // }
+    target:'https://click.suning.cn',
+    changeOrigin:true
 }))
 
-app.listen('5000',() => {
+app.listen('9000',() => {
     console.log("static")
 })
-
-
-// const express = require("express")
-
-// const app = express()
-// app.use(express.static(__dirname+'/static'))
-// app.listen('5000',() => {
-//     console.log("static")
-// })
