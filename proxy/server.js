481ba262f3bde27406b385b7b293780a18acc1c9
Sean <lleiiell@live.com>
2021-12-18 18:32:35 +0800

    update

--- proxy/server.js
+++ proxy/server.js
@@ -1,13 +1,59 @@
 const express = require("express")
-const {createProxyMiddleware} = require("http-proxy-middleware")
+//const {createProxyMiddleware} = require("http-proxy-middleware")
+const https = require('https')
 
 const app = express()
 app.use(express.static(__dirname+'/'))
 
-app.use('^/sa',createProxyMiddleware({
-    target:'https://click.suning.cn',
-    changeOrigin:true
-}))
+// app.use('^/sa',createProxyMiddleware({
+//     target:'https://click.suning.cn',
+//     changeOrigin:true
+// }))
+app.get('/sa/jsConfig.action',function(req,res,next){
+    new Promise(function(resolve,reject){
+        https.get('https://click.suning.cn'+req.url, (res) => {
+            const { statusCode } = res;
+            const contentType = res.headers['content-type'];
+
+            let error;
+            // Any 2xx status code signals a successful response but
+            // here we're only checking for 200.
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                                `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                                `Expected application/json but received ${contentType}`);
+            }
+            if (error) {
+                console.error(error.message);
+                // Consume response data to free up memory
+                res.resume();
+                return;
+            }
+
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    resolve(parsedData)
+                } catch (e) {
+                    reject(e)
+                console.error(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+            reject(e)
+        });
+    }).then((resData)=>{
+        res.send(resData)
+    }).catch((e)=>{
+        console.log(e)
+    })
+})
 
 app.listen('9000',() => {
     console.log("static")
--- src/serverStatic.js
+++ src/serverStatic.js
@@ -1,7 +1,38 @@
+const { response } = require("express")
 const express = require("express")
 
 const app = express()
+app.all('*',(req,res,next) => {
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","Content-Type,Content-Length,Authorization,Accept,X-Requested-With")
+    res.header("Access-Control-Allow-Method","POST,GET,OPTIONS")
+    res.header("Content-Type", "application/json;charset=utf-8");
+    
+    if(req.method === 'OPTIONS'){
+        res.send(200)
+    }else{
+        next()
+    }
+})
 app.use(express.static(__dirname+'/static'))
+app.get('/user/list',function(req,res,next){
+    let resData = {
+        'responseCode' : '0000',
+        'data':[],
+        'total':10
+
+    }
+    res.send(JSON.stringify(resData));
+})
+app.put('/user/list',function(req,res,next){
+    res.send('操作成功,你真棒!')
+})
+app.post('/user/list',function(req,res,next){
+    res.send('操作成功,你真棒')
+})
+app.delete('/user/list',function(req,res,next){
+    res.send('操作成功,你真棒')
+})
 app.listen('5000',() => {
     console.log("static")
 })
