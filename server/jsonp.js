b2900eeb15346b4e73c45cf2fe86f81590a13509
Sean <lleiiell@live.com>
2021-12-19 22:24:31 +0800

    update

--- server/jsonp.js
+++ server/jsonp.js
@@ -6,7 +6,12 @@ app.get('/user/list',(req,res,next) => {
     let {callback,pageNo,pageSize} =  req.query;
     console.log(pageNo,pageSize)
 
-    let resData = [{'userId':"001"},{'userId':"002"},{'userId':"003"},{'userId':"004"}]
+    let resData = {
+        responseCode:"0000",
+        data:[{'userId':"001"},{'userId':"002"},{'userId':"003"},{'userId':"004"}],
+        total:10,
+        responseMsg:"请求成功"
+    }
     res.send(`${callback}(${JSON.stringify(resData)})`)
 })
 
